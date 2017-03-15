1981730f269bf4236c29e2d77a2cc8f7aa444c72
Jan Wong <janwong94@gmail.com>
2017-03-15 04:29:06 +0000

    add input form

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; //default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -39,15 +42,42 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+//add page for url input form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+
 //add page for displaying a single URL and its shortened form
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
 
   let templateVars = { shortURL, longURL }
-
   res.render("urls_show", templateVars);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
