6b6600c94c888dda733f5cf5155615b4de007811
Jan Wong <janwong94@gmail.com>
2017-03-17 19:57:45 +0000

    add user specific features

--- express_server.js
+++ express_server.js
@@ -14,12 +14,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xk": "http://www.google.com"
-};
-
-
 const users = {
   "userRandomID" : {
     id: "userRandomID",
@@ -34,9 +28,45 @@ const users = {
   "user3RandomID": {
     id: "user3RandomID",
     email: "user3@example.com",
-    password: "funny-bunny"
+    password: "funny-bunny",
   }
 };
+
+const urlDatabase = {
+  "user3RandomID": {
+    'd;lkjt6': 'https://example.com/wow_wow_wow',
+    'asdf': 'http://google.ca'
+  },
+  "userRandomID" : {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xk": "http://www.google.com"
+  },
+  "user2RandomID" : {
+    'qqqq': 'http://zombo.com'
+  }
+};
+
+  //   "b2xVn2": {
+  //     url: "http://www.lighthouselabs.ca",
+  //     user: ""
+  //   }
+  //   "9sm5xk": "http://www.google.com"
+  // }
+
+
+function addUrlToUser(userId, shortUrl, longUrl) {
+
+  let userUrls = urlDatabase[userId];
+
+  if (!userUrls) {
+    userUrls = {};
+    userUrls[shortUrl] = longUrl;
+    urlDatabase[userId] = userUrls;
+  } else {
+    userUrls[shortUrl] = longUrl;
+  }
+}
+
 
 //function to generate 6 random numbers and letters
 function generateRandomString() {
@@ -63,21 +93,6 @@ function addUser(email, password) {
 }
 
 
-
-
-// app.param('shortURL', (req, res, next) => {
-//   console.log('In params middleware');
-//   res.locals.shortURL = req.params.shortURL;
-
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log('In time middleware');
-//   res.locals.currentTime = new Date();
-//   next();
-// })
-
 //add endpoint for root directory
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -87,17 +102,16 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 //add route with html response
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n")
-});
+
 //add route handler for urls
 app.get("/urls", (req, res) => {
 
   //read the value of the cookie
 
+  let user_id = req.cookies["user_id"];
   let templateVars = {
-    urls: urlDatabase,
-    user: req.cookies["user_id"]
+    urls: urlDatabase[user_id],
+    user: user_id
   };
   res.render("urls_index", templateVars);
   //res.render("urls_index",{user:flag});
@@ -105,9 +119,10 @@ app.get("/urls", (req, res) => {
 
 //add page for url input form
 app.get("/urls/new", (req, res) => {
+  let user_id = req.cookies["user_id"];
   let templateVars = {
-    urls: urlDatabase,
-    user: req.cookies["user_id"]
+    urls: urlDatabase[user_id],
+    user: user_id
   };
 
   if(req.cookies["user_id"]) {
@@ -117,7 +132,7 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
-//log out the (longurl) link that user input into the form
+//console log out the (longurl) link that user input into the form
 app.post("/urls", (req, res, next) => {
   console.log("req.body: ", req.body);
 
@@ -125,7 +140,8 @@ app.post("/urls", (req, res, next) => {
   let shortURL = generateRandomString();
 
   //add new longurl & short url to the urldatabse obj
-  urlDatabase[shortURL] = longURL;
+  // urlDatabase[shortURL] = longURL;
+  addUrlToUser(req.cookies["user_id"], shortURL, longURL);
 
   //redirect page
   res.redirect('/urls/'+shortURL);
@@ -133,25 +149,43 @@ app.post("/urls", (req, res, next) => {
 
 //if user inputs short url send them to the long url website
 app.get("/u/:shortURL", (req, res) => {
-  res.redirect(urlDatabase[req.params.shortURL]);
+  for (var userId in urlDatabase) {
+    let longURL = urlDatabase[userId][req.params.shortURL];
+      console.log("FOUND IT", urlDatabase);
+      console.log("shortURL:", req.params.shortURL);
+      console.log("longURL:", longURL);
+    if (longURL !== undefined) {
+      res.redirect(longURL);
+      return;
+    }
+  }
+  res.status(404).send('url does not exist');//must be outside for loop or else it will loop around
 })
+
+
 
 //add page for displaying a single URL and its shortened form
 app.get("/urls/:shortURL", (req, res) => {
+  let user_id = req.cookies["user_id"];
+
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
+  let longURL = urlDatabase[user_id][shortURL];
 
   let templateVars = {
     shortURL,
     longURL,
-    user: req.cookies["user_id"]
+    user: users[user_id]
   }
   res.render("urls_show", templateVars);
 });
 
 //delete to remove exisitng shortened uRLS from database
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
+
+
+  // delete urlDatabase[req.params.shortURL];
+  delete urlDatabase[req.cookies.user_id][req.params.shortURL];
+
 
   //after delete redirect back to urls_index page
   res.redirect('/urls');
@@ -160,8 +194,9 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 //update to exist existing urls
 app.post("/urls/:shortURL", (req, res) => {
   //make the long url in object = to long url that was input in the form
-  urlDatabase[req.params.shortURL] = req.body.longURL;
-
+  // urlDatabase[req.params.shortURL] = req.body.longURL;
+  urlDatabase[req.cookies.user_id][req.params.shortURL] = req.body.longURL;
+  // addUrlToUser(req.cookies["user_id"], req.params.shortUrl, req.body.longURL);
   //after updating, redirect client back to index page
   res.redirect('/urls');
 });
